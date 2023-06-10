44081957a404160ec9aa33b423d169d21787e015
Dushko Avramoski <Dushko.Avramoski@netcetera.com>
2023-06-10 11:25:43 +0200

    List view with cards

--- lib/main.dart
+++ lib/main.dart
@@ -5,65 +5,94 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final Color primaryColor = Colors.teal;
+
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Student organizer',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: primaryColor as MaterialColor,
+        primaryColor: primaryColor,
+        primaryColorLight: (primaryColor as MaterialColor)[200],
+        primaryColorDark: (primaryColor as MaterialColor)[700],
+        colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page', elements: [
+        "Prv",
+        "Vtor",
+        "Tret",
+        "Cetvrt",
+        "Pet",
+        "Shest",
+        "Tret",
+        "Cetvrt",
+        "Pet",
+        "Shest"
+      ]),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key, required this.title, required this.elements});
 
   final String title;
+  final List<String> elements;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        actions: [
+          IconButton(
+              onPressed: () => print('asd'),
+              icon: const Icon(Icons.account_circle_outlined))
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () => print("sad"),
+      ),
+      body: ListView.builder(
+        itemCount: widget.elements.length,
+        itemBuilder: (context, index) {
+          return Card(
+              child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.all(20),
+                child: Text(
+                  widget.elements.elementAt(index),
+                  style: TextStyle(
+                      fontSize: 30, color: Theme.of(context).primaryColorLight),
+                ),
+              ),
+              IconButton(
+                icon: const Icon(Icons.add_box_rounded),
+                onPressed: () {
+                  print(index);
+                },
+              )
+            ],
+          ));
+        },
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
